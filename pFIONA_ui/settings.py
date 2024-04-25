e1274beeab073a826d7a80420af684aea1551cf3
Warkild <alexandre17.jaud@gmail.com>
2024-04-25 09:54:39 -0700

    Install tailwindcss
    Installing the tailwind css library

--- pFIONA_ui/settings.py
+++ pFIONA_ui/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pFIONA_auth'
+    'pFIONA_auth',
+    'tailwind',
+    'theme'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+NPM_BIN_PATH = "E:\\nodejs\\npm.cmd"
+
