6c8bac24eb34f7879b8cabd7d5459b77ebab9ec0
Warkild <alexandre17.jaud@gmail.com>
2024-04-29 10:10:35 -0700

    Create sensor model

--- pFIONA_sensors/models.py
+++ pFIONA_sensors/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Sensor(models.Model):
+    name = models.CharField(max_length=100)
+    ip_address = models.GenericIPAddressField()
+
+    class Meta:
+        db_table = 'pfiona_sensor'
--- pFIONA_sensors/views.py
+++ pFIONA_sensors/views.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from pFIONA_sensors.models import Sensor
+
 
 @login_required()
 def sensors_list(request):
-    return render(request, 'pFIONA_sensors/list.html', {'current_path': request.path})
+    sensors_list = Sensor.objects.all()
+    context = {
+        'sensors_list': sensors_list,
+        'current_path': request.path,
+    }
+    return render(request, 'pFIONA_sensors/list.html', context=context)
+
 
