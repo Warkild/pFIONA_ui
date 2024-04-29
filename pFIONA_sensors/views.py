6a97965c8dd68598dd72300b9f33c0ec54fbed2f
Warkild <alexandre17.jaud@gmail.com>
2024-04-29 14:22:31 -0700

    Modify add sensor form page
    Create form into forms.py and update view.py to manage sensor creation

--- pFIONA_sensors/views.py
+++ pFIONA_sensors/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import SensorForm
 from pFIONA_sensors.models import Sensor
 
 
@@ -15,4 +15,11 @@ def sensors_list(request):
 
 
 def sensors_add(request):
-    return render(request, 'pFIONA_sensors/sensor_add.html')
+    if request.method == 'POST':
+        form = SensorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('sensors_list')
+    else:
+        form = SensorForm()
+    return render(request, 'pFIONA_sensors/sensor_add.html', {'form': form})
