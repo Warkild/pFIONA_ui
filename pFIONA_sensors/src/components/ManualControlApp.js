bca5ebaf8ae138abd5f1014953e1275424ee2f1e
Warkild <alexandre17.jaud@gmail.com>
2024-05-01 16:23:52 -0700

    Create brick log v1 in manual view

--- pFIONA_sensors/src/components/ManualControlApp.js
+++ pFIONA_sensors/src/components/ManualControlApp.js
@@ -1,36 +1,37 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import React, { Component, useState } from "react";
+import { createRoot } from "react-dom/client";
 import ValvePort from "./plugins/ValvePort";
-import {createRoot} from "react-dom/client";
 import Pumps from "./plugins/Pumps";
 import Spectrophotometer from "./plugins/Spectrophotometer";
 import AuxPump from "./plugins/AuxPump";
 import PreEstablishedScan from "./plugins/PreEstablishedScan";
+import Log from "./plugins/Log";
 
-export default class ManualControlApp extends Component {
-  constructor(props) {
-    super(props);
-  }
+function ManualControlApp() {
+  const [logMessages, setLogMessages] = useState([]);
 
-  render() {
-    return (
-        <div className={"flex flex-col"}>
-            <div className={"flex flex-row justify-between pb-12"}>
-                <ValvePort/>
-                <Pumps/>
-            </div>
-            <div className={"flex flex-row justify-between pb-12"}>
-                <Spectrophotometer/>
-                <AuxPump/>
-            </div>
-            <div className={"flex flex-row justify-between"}>
-                <PreEstablishedScan/>
-            </div>
-        </div>
+  const addLogMessage = (message) => {
+    setLogMessages(prev => [...prev, message]);
+  };
 
-
-    );
-  }
+  return (
+      <div className={"flex flex-col"}>
+          <div className={"flex flex-row justify-between pb-12"}>
+              <Log messages={logMessages}/>
+          </div>
+          <div className={"flex flex-row justify-between pb-12"}>
+              <ValvePort/>
+              <Pumps/>
+          </div>
+          <div className={"flex flex-row justify-between pb-12"}>
+              <Spectrophotometer addLogMessage={addLogMessage}/>
+              <AuxPump/>
+          </div>
+          <div className={"flex flex-row justify-between"}>
+              <PreEstablishedScan/>
+          </div>
+      </div>
+  );
 }
 
 const appDiv = document.getElementById("app");
