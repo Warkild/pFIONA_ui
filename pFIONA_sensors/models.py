9586a8283831ec4ccb39c8086945b8711b66a35a
Warkild <alexandre17.jaud@gmail.com>
2024-05-02 15:47:52 -0700

    Create model reagent

--- pFIONA_sensors/models.py
+++ pFIONA_sensors/models.py
@@ -9,3 +9,14 @@ class Sensor(models.Model):
 
     class Meta:
         db_table = 'pfiona_sensor'
+
+
+class Reagent(models.Model):
+    name = models.CharField(max_length=100)
+    volume = models.IntegerField()
+    max_volume = models.IntegerField()
+    port = models.IntegerField(null=True)
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'pfiona_reagent'
