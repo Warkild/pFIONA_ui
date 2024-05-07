02d80ef32c71c2c30938c6158671f612c6d709ca
Warkild <alexandre17.jaud@gmail.com>
2024-05-07 16:12:39 -0700

    Stock JWT token in session storage

--- pFIONA_auth/urls.py
+++ pFIONA_auth/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='pFIONA_auth/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='pFIONA_auth/logged_out.html'), name='logout'),
+    path('jwt/', views.get_jwt_tokens, name='get_jwt_tokens'),
 ]
--- pFIONA_ui/settings.py
+++ pFIONA_ui/settings.py
@@ -160,6 +160,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'pFIONA_auth.serializers.CustomTokenObtainPairSerializer',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
 }
