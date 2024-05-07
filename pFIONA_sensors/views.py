07fe1bbaddbcd968228191c43c4005dd2677b76e
Warkild <alexandre17.jaud@gmail.com>
2024-05-07 14:19:49 -0700

    Clean code fix

--- pFIONA_sensors/views.py
+++ pFIONA_sensors/views.py
@@ -4,14 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
-
-from .forms import SensorForm, SensorNameAndNotesForm, ReagentEditForm
-from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
-from pFIONA_auth.serializers import CustomTokenObtainPairSerializer
-from django.db.models import Q
 
+from pFIONA_auth.serializers import CustomTokenObtainPairSerializer
 from pFIONA_sensors.models import Sensor, Reagent
+from .forms import SensorForm, SensorNameAndNotesForm, ReagentEditForm
 
 
 @login_required()
@@ -49,8 +46,8 @@ def sensors_add(request):
 
 
 @login_required()
-def sensors_manual(request, id):
-    sensor = get_object_or_404(Sensor, pk=id)
+def sensors_manual(request, sensor_id):
+    sensor = get_object_or_404(Sensor, pk=sensor_id)
 
     # JWT Token Loading
 
@@ -62,24 +59,24 @@ def sensors_manual(request, id):
     refresh_token = str(refresh)
 
     return render(request, 'pFIONA_sensors/view/sensors_manual.html',
-                  {'id': id, 'ip_address': sensor.ip_address, 'access_token': access_token,
+                  {'id': sensor_id, 'ip_address': sensor.ip_address, 'access_token': access_token,
                    'refresh_token': refresh_token})
 
 
 @login_required()
-def sensors_deploy(request, id):
-    return render(request, 'pFIONA_sensors/view/sensors_deploy.html', {'id': id})
+def sensors_deploy(request, sensor_id):
+    return render(request, 'pFIONA_sensors/view/sensors_deploy.html', {'id': sensor_id})
 
 
 @login_required()
-def sensors_data(request, id):
-    return render(request, 'pFIONA_sensors/view/sensors_data.html', {'id': id})
+def sensors_data(request, sensor_id):
+    return render(request, 'pFIONA_sensors/view/sensors_data.html', {'id': sensor_id})
 
 
 @login_required()
-def sensors_reagents(request, id):
-    sensor = get_object_or_404(Sensor, pk=id)
-    reagents = Reagent.objects.filter(sensor_id=id)
+def sensors_reagents(request, sensor_id):
+    sensor = get_object_or_404(Sensor, pk=sensor_id)
+    reagents = Reagent.objects.filter(sensor_id=sensor_id)
 
     # JWT Token Loading
 
@@ -102,7 +99,7 @@ def sensors_reagents(request, id):
     reagents_json = json.dumps(reagents_data)
 
     return render(request, 'pFIONA_sensors/view/sensors_reagents.html', {
-        'id': id,
+        'id': sensor_id,
         'ip_address': sensor.ip_address,
         'reagents_json': reagents_json,
         'access_token': access_token,
@@ -112,18 +109,18 @@ def sensors_reagents(request, id):
 
 @login_required()
 @csrf_exempt
-def sensors_reagents_valve_update(request, id):
+def sensors_reagents_valve_update(request, sensor_id):
     if request.method == 'POST':
         try:
             data = json.loads(request.body.decode('utf-8'))
 
             print(data)
 
-            Reagent.objects.filter(sensor_id=id).update(port=None)
+            Reagent.objects.filter(sensor_id=sensor_id).update(port=None)
 
             for index, reagent_id in enumerate(data):
                 if reagent_id != 'none':
-                    Reagent.objects.filter(id=int(reagent_id)).update(port=index+1)
+                    Reagent.objects.filter(id=int(reagent_id)).update(port=index + 1)
 
             return JsonResponse({'status': 'success', 'message': 'Ports updated successfully'})
         except Exception as e:
@@ -133,42 +130,42 @@ def sensors_reagents_valve_update(request, id):
 
 
 @login_required()
-def sensors_reagent_delete(request, id, reagent_id):
+def sensors_reagent_delete(request, sensor_id, reagent_id):
     reagent = get_object_or_404(Reagent, pk=reagent_id)
     reagent.delete()
-    return redirect('sensors_reagents', id=id)
+    return redirect('sensors_reagents', id=sensor_id)
 
 
 @login_required()
-def sensors_reagent_edit(request, id, reagent_id):
+def sensors_reagent_edit(request, sensor_id, reagent_id):
     reagent = get_object_or_404(Reagent, pk=reagent_id)
     reagent_form = ReagentEditForm(request.POST or None, instance=reagent, prefix='reagent')
 
     if request.method == 'POST':
         print('POST')
-        if 'sumbit_reagent' in request.POST:
-            print('sumbit reagent')
+        if 'submit_reagent' in request.POST:
+            print('submit reagent')
             reagent_form = ReagentEditForm(request.POST, instance=reagent, prefix='reagent')
             if reagent_form.is_valid():
                 reagent_form.save()
-                return redirect('sensors_reagents', id=id)
+                return redirect('sensors_reagents', id=sensor_id)
     return render(request, 'pfiONA_sensors/view/sensors_reagent_edit.html', {
-        'id': id,
+        'id': sensor_id,
         'reagent_form': reagent_form,
     })
 
 
 @login_required
-def sensors_settings(request, id):
-    sensor = get_object_or_404(Sensor, pk=id)
+def sensors_settings(request, sensor_id):
+    sensor = get_object_or_404(Sensor, pk=sensor_id)
     name_notes_form = SensorNameAndNotesForm(request.POST or None, instance=sensor, prefix='name_notes')
 
     if request.method == 'POST':
-        if 'sumbit_name_notes' in request.POST:
+        if 'submit_name_notes' in request.POST:
             name_notes_form = SensorNameAndNotesForm(request.POST, instance=sensor, prefix='name_notes')
             if name_notes_form.is_valid():
                 name_notes_form.save()
-                return redirect('sensors_settings', id=id)
+                return redirect('sensors_settings', id=sensor_id)
 
     return render(request, 'pFIONA_sensors/view/sensors_settings.html',
-                  {'id': id, 'name_notes_form': name_notes_form, 'sensor': sensor})
+                  {'id': sensor_id, 'name_notes_form': name_notes_form, 'sensor': sensor})
