ef68e9a8ecee6975320519be1089da6f2cf0f470
Warkild <alexandre17.jaud@gmail.com>
2024-06-04 15:28:04 -0700

    Update model
    Add cycle in spectrum

--- pFIONA_sensors/models.py
+++ pFIONA_sensors/models.py
@@ -68,6 +68,7 @@ class Spectrum(models.Model):
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, name="pfiona_sensor")
     time = models.ForeignKey('Time', on_delete=models.CASCADE, name="pfiona_time")
     spectrumtype = models.ForeignKey('SpectrumType', on_delete=models.CASCADE, name="pfiona_spectrumtype")
+    cycle = models.IntegerField(null = True)
 
     class Meta:
         db_table = 'pfiona_spectrum'
