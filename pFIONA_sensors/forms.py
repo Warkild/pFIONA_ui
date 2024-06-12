5adabbad19f9415e0223e5f2fba0679dc3dda6fb
Warkild <alexandre17.jaud@gmail.com>
2024-06-12 14:34:48 -0700

    Update labels for sensor settings

--- pFIONA_sensors/forms.py
+++ pFIONA_sensors/forms.py
@@ -30,10 +30,22 @@ class SensorSettingsForm(ModelForm):
     class Meta:
         model = Sensor
         fields = [
-            'boxcar_width', 'time_to_wait_for_lamp', 'time_between_2_spectrophotometer_scan',
-            'flush_flow_rate', 'flush_volume', 'max_flow_rate', 'max_aspirate_volume', 'volume_for_valve_port_priming',
-            'scans_to_average', 'time_to_pump'
+            'boxcar_width', 'scans_to_average', 'time_to_wait_for_lamp', 'time_between_2_spectrophotometer_scan',
+            'flush_flow_rate', 'flush_volume', 'max_flow_rate', 'max_aspirate_volume', 'volume_for_valve_port_priming', 'time_to_pump'
         ]
+
+        labels = {
+            'boxcar_width': 'Boxcar width (number of neighboring pixels used to average each pixel)',
+            'time_to_wait_for_lamp': 'Time to wait for lamp to be at full intensity (s)',
+            'time_between_2_spectrophotometer_scan': 'Minimum time between 2 spectrophotometer scan (s)',
+            'flush_flow_rate': 'Flush flow rate (μL/s)',
+            'flush_volume': 'Flush volume (μL)',
+            'max_flow_rate': 'Max flow rate (μL/s)',
+            'max_aspirate_volume': 'Max aspirate volume (μL)',
+            'volume_for_valve_port_priming': 'Volume for valve port priming (μL)',
+            'scans_to_average': 'Scans to average (number of scan take for averaging)',
+            'time_to_pump': 'Time to pump sample from ocean (s)'
+        }
 
     def clean(self):
         cleaned_data = super().clean()
